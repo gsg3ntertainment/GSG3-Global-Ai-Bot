198d9c61bf9bb46eab434612a25627e3ab7affcd
SeeTrick <tazhminwilliams@yahoo.com>
2025-02-20 05:57:15 -0500

    Update twitch_bot.js
    Signed-off-by: SeeTrick <tazhminwilliams@yahoo.com>

--- twitch_bot.js
+++ twitch_bot.js
@@ -1,7 +1,7 @@
 import tmi from 'tmi.js';
 import OpenAI from 'openai';
 import mysql from 'mysql2/promise';
-const dotenv = require('dotenv');
+const dotenv = import('dotenv');
 
 // Load environment variables
 dotenv.config();
